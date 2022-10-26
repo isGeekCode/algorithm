245e702c5af01b2466b360103d30b2d6523dbc61
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-10-26 14:45:58 +0900

    [ADD] BJ_10926

---
+++ Algorithm/Algorithm/InputAndOperation/BJ_10926.swift
@@ -0,0 +1,16 @@
+//
+//  BJ_10926.swift
+//  Algorithm
+//
+//  Created by bang_hyeonseok on 2022/10/26.
+//
+
+import Foundation
+/**
+입력: joonas
+출력: joonas??!
+
+*/
+
+//var arr = readLine()!
+//print(arr,"??!")
--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -7,22 +7,5 @@
 
 import Foundation
 
-// MARK: - lv.1
-//let input = readLine()!
-//let intArr = input.split(separator: " ").map{ Int($0)! }
-//let a = intArr[0]
-//let b = intArr[1]
-//print(a * b)
-
-// MARK: - lv.2
-//let input = readLine()!
-//let intArr = input.split(separator: " ").map{ Int($0)! }
-//print(intArr[0]*intArr[1])
-
-// MARK: - lv.3
-//var arr = readLine()!.split(separator: " ").map { Int($0)! }
-//print(arr[0] + arr[1])
-//print(arr[0] - arr[1])
-//print(arr[0] * arr[1])
-//print(arr[0] / arr[1])
-//print(arr[0] % arr[1])
+let arr: String = readLine()!
+print("\(arr)??!")
