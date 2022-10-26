d259a4f4fbb0fed10e6c0932358b56b69e66921f
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-10-26 14:24:22 +0900

    [ADD]  BJ_10869 사칙연산

---
+++ Algorithm/Algorithm/InputAndOperation/BJ_10869.swift
@@ -0,0 +1,27 @@
+//
+//  BJ_10869.swift
+//  Algorithm
+//
+//  Created by bang_hyeonseok on 2022/10/26.
+//
+
+import Foundation
+
+/**
+ 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+ 입력: 7 3
+ 출력:
+ 10
+ 4
+ 21
+ 2
+ 1
+ */
+
+//var arr = readLine()!.split(separator: " ").map { Int($0)! }
+//print(arr[0] + arr[1])
+//print(arr[0] - arr[1])
+//print(arr[0] * arr[1])
+//print(arr[0] / arr[1])
+//print(arr[0] % arr[1])
