5785a7a4dc77225ea7934de16e2c59db80b10454
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-10-26 14:03:56 +0900

    [CHORE] 기존문제 정리

--- Algorithm/Algorithm/InputAndOperation/BJ_1000.swift
+++ Algorithm/Algorithm/InputAndOperation/BJ_1000.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-
+/**
+ 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
 
 // MARK: - lv.1
 //let input = readLine()!
--- Algorithm/Algorithm/InputAndOperation/BJ_10098.swift
+++ Algorithm/Algorithm/InputAndOperation/BJ_10098.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 
-let line = readLine()!
-let lineArr = line.components(separatedBy: " ")
-let a = Int(lineArr[0])!
-let b = Int(lineArr[1])!
-print(a+b)
+/**
+ 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+ */
+
+// MARK: - Lv.1
+//let line = readLine()!
+//let lineArr = line.components(separatedBy: " ")
+//let a = Int(lineArr[0])!
+//let b = Int(lineArr[1])!
+//print(a*b)
+
+// MARK: - Lv.3
+//var arr = readLine()!.split(separator: " ").map { Int($0)! }
+//print(arr[0] * arr[1])
