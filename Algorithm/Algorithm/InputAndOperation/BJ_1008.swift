cbf09e842d45e149b2cb6eb81160b82dd5b046ab
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-10-26 14:04:51 +0900

    [ADD] BJ1008

--- Algorithm/Algorithm/InputAndOperation/BJ_1008.swift
+++ Algorithm/Algorithm/InputAndOperation/BJ_1008.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+/**
+ 두 정수 A와 B를 입력받은 다음, A / B를 출력하는 프로그램을 작성하시오.
+ 
+ 입력: "1 3"
+ 출력: 0.33333333333333333333333333333333
+
+ 입력: "4 5"
+ 출력: 0.8
+ */
+
+//var arr = readLine()!.split(separator: " ").map { Double($0)! }
+//print(arr[0] / arr[1])
--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
+//  BJ_1000.swift
 //  Algorithm
 //
 //  Created by bang_hyeonseok on 2022/10/26.
 //
 
 import Foundation
+
+// MARK: - lv.1
+//let input = readLine()!
+//let intArr = input.split(separator: " ").map{ Int($0)! }
+//let a = intArr[0]
+//let b = intArr[1]
+//print(a * b)
+
+// MARK: - lv.2
+//let input = readLine()!
+//let intArr = input.split(separator: " ").map{ Int($0)! }
+//print(intArr[0]*intArr[1])
+
+// MARK: - lv.3
+var arr = readLine()!.split(separator: " ").map { Double($0)! }
+print(arr[0] / arr[1])
