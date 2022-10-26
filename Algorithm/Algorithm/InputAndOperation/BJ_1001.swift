9c7e2219c2c3e0b873c5c5b4e0204ec1136a2a15
bang.hyeonseok <bang_hyeonseok@radcns.com>
2022-10-26 14:04:29 +0900

    [ADD]  BJ1001

--- Algorithm/Algorithm/InputAndOperation/BJ_1001.swift
+++ Algorithm/Algorithm/InputAndOperation/BJ_1001.swift
@@ -7,25 +7,22 @@
 
 import Foundation
 
+/**
+ 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ */
 
-//var arr = readLine()!.split(separator: " ").map { Int($0)! }
-//print(arr[0] - arr[1])
-
-
-//let input = readLine()!
-//let intArr = input.split(separator: " ").map{ Int($0)! }
-//print(intArr[0]-intArr[1])
-
+// MARK: - lv.1
 //let input = readLine()!
 //let intArr = input.split(separator: " ").map{ Int($0)! }
 //let a = intArr[0]
 //let b = intArr[1]
 //print(a - b)
 
+// MARK: - lv.2
+//let input = readLine()!
+//let intArr = input.split(separator: " ").map{ Int($0)! }
+//print(intArr[0]-intArr[1])
 
+// MARK: - lv.3
 //var arr = readLine()!.split(separator: " ").map { Int($0)! }
-//print(arr[0] - arr[1])
-//
-//var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
-//
 //print(arr[0] - arr[1])
