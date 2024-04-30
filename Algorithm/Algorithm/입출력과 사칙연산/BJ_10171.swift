45b06033be3ee6ab314814546e95cda56faab2c4
isGeekcode <h1guitar@naver.com>
2024-04-30 14:58:13 +0900

    input and statement

---
+++ Algorithm/Algorithm/입출력과 사칙연산/BJ_10171.swift
@@ -0,0 +1,61 @@
+//
+//  BJ_10171.swift
+//  Algorithm
+//
+//  Created by 방현석 on 4/30/24.
+//
+
+import Foundation
+// MARK: 고양이
+
+
+/**
+ 
+ - 문제
+ 
+ 아래 예제와 같이 고양이를 출력하시오.
+
+
+ 
+ - 입력
+ 없음.
+
+
+ 
+ - 예제 입력 : 없음
+
+ 
+ - 출력
+ 고양이를 출력한다
+ 
+ - 예제 출력 :
+
+ */
+
+/*
+ \    /\
+  )  ( ')
+ (  /  )
+  \(__)|
+ */
+
+//let t = """
+//        \\    /\\
+//         )  ( ')
+//        (  /  )
+//         \\(__)|
+//        """
+//
+//print(t)
+
+
+// MARK: 새로운 답
+
+// 원본을 유지한상태로 치환하는 방법
+
+//// 백슬래시 치환
+//let escapedCatString = t.replacingOccurrences(of: "\", with: "\\")
+//
+//// 출력
+//print(escapedCatString)
+
