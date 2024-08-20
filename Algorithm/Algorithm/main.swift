b0b23e6afc145be4a89b86ec9b241c2b850353d9
isGeekCode <h1guitar@naver.com>
2024-08-21 00:08:26 +0900

    algo

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,48 +1,53 @@
 import Foundation
 
 /*
- 모든 자연수 x에 대해서 현재 값이 x이면 x가 짝수일 때는 2로 나누고, x가 홀수일 때는 3 * x + 1로 바꾸는 계산을 계속해서 반복하면 언젠가는 반드시 x가 1이 되는지 묻는 문제를 콜라츠 문제라고 부릅니다.
+ 정수 배열 arr가 주어집니다. arr를 이용해 새로운 배열 stk를 만드려고 합니다.
 
- 그리고 위 과정에서 거쳐간 모든 수를 기록한 수열을 콜라츠 수열이라고 부릅니다.
+ 변수 i를 만들어 초기값을 0으로 설정한 후 i가 arr의 길이보다 작으면 다음 작업을 반복합니다.
 
- 계산 결과 1,000 보다 작거나 같은 수에 대해서는 전부 언젠가 1에 도달한다는 것이 알려져 있습니다.
-
- 임의의 1,000 보다 작거나 같은 양의 정수 n이 주어질 때 초기값이 n인 콜라츠 수열을 return 하는 solution 함수를 완성해 주세요.
- - n : 10
- - result : [10, 5, 16, 8, 4, 2, 1]
-
+ 만약 stk가 빈 배열이라면 arr[i]를 stk에 추가하고 i에 1을 더합니다.
+ stk에 원소가 있고, stk의 마지막 원소가 arr[i]보다 작으면 arr[i]를 stk의 뒤에 추가하고 i에 1을 더합니다.
+ stk에 원소가 있는데 stk의 마지막 원소가 arr[i]보다 크거나 같으면 stk의 마지막 원소를 stk에서 제거합니다.
+ 위 작업을 마친 후 만들어진 stk를 return 하는 solution 함수를 완성해 주세요.
+ 
+ arr    result
+ [1, 4, 2, 5, 3]    [1, 2, 3]
  */
 
 
 /// 1. 아이디어
-/// - 현재값 x 체크
-///     - 짝수 : even -> x / 2
-///     - 홀수 : odd -> 3 * x + 1
-/// - 현재값 x 체크
+/// - i < arr의 길이 보다 작은 경우 : 아래 과정 반복
+///     - stk가 빈배열인경우
+///         - arr[i]를 stk에 추가
+///         - i += 1
+///     - stk에 원소가 있는 경우
+///         - stk.last < arr[i]
+///             - stk.append(arr[i])
+///             - i += 1
+///         - stk.last >= arr[i]
+///             - stk의 마지막 원소를 stk에서 제거 ::drop.last??
 /// 2. 시간복잡도
 /// 3. 자료구조
+/// - i: Int => 변수
 
 
 
-
-func solution(_ n:Int) -> [Int] {
-    var current = n
-    var answers = [current]
+func solution(_ arr:[Int]) -> [Int] {
+    var stk = [Int]()
+    var i = 0
     
-    while current != 1 {
-        if current % 2 != 0 { // odd
-            print("odd")
-            current = (current * 3) + 1
-        } else if current % 2 == 0 { // even
-            print("!odd")
-            current = current / 2
+    while i < arr.count {
+        if stk.isEmpty || (stk.last! < arr[i]) {
+            stk.append(arr[i])
+            i += 1
+        } else {
+            stk.removeLast()
         }
-        answers.append(current)
     }
-    return answers
+    return stk
 }
-
-print(solution(10))
+let input = [1, 4, 2, 5, 3]
+print(solution(input))
 
 // MARK: <#제목#>
 
